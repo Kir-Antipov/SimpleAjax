72a28355b39e114856c519efeac1811f1ce71768
Kir-Antipov <kp_antipov@mail.ru>
2020-04-02 11:06:07 +0300

    Added UMD

--- src/simpleAjax.js
+++ src/simpleAjax.js
@@ -91,7 +91,7 @@ class AjaxResult {
 
 const ajax = (function () {
 
-    const Request = XMLHttpRequest;
+    const Request = typeof XMLHttpRequest === "undefined" ? require("xmlhttprequest").XMLHttpRequest : XMLHttpRequest;
 
     if (!Request)
         throw new Error("XMLHttpRequest is unavailable");
@@ -309,3 +309,15 @@ HTMLFormElement.prototype.addAjax = function(handler, options) {
     this.addEventListener("submit", e => e.preventDefault());
     this.addEventListener("submit", eventHandler);
 };
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object') {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
+  }
+}(this, function () {
+    return ajax;
+}));
